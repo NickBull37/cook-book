b9e097dd2eff1c356cd7217a71d9ad30321af175
Nick <nbulisky@gmail.com>
2025-05-13 19:42:12 -0400

    Fixing react routing for pages deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Box, CssBaseline } from '@mui/material';
 import { CookBookLanding, CookBookDashboard, Recipe } from "./components";
 
 function App() {
     return (
-        <BrowserRouter>
+        <Box>
             <Routes>
                 <Route path="/" element={<CookBookLanding />} />
                 <Route path="/dashboard" element={<CookBookDashboard />} />
                 <Route path="/recipe" element={<Recipe />} />
             </Routes>
-        </BrowserRouter>
+        </Box>
     );
 }
 
--- src/components/pages/CookBookLanding.jsx
+++ src/components/pages/CookBookLanding.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import { Box, Stack, Typography, Grid, Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import { Navbar, RecipeMap } from '../../components';
+
+const PageContainer = styled(Box)(() => ({
+    marginTop: '8rem',
+}));
+
 const CookBookLanding = () => {
     return (
-        <div>CookBookLanding</div>
+        <Stack>
+            <Navbar showReturnLink={false} />
+            <PageContainer>
+                <Button>
+                    Enter
+                </Button>
+            </PageContainer>
+        </Stack>
     );
 }
 
