40f242c090952fa0c411e5ee1880abd90b571eaf
Nick <nbulisky@gmail.com>
2025-05-12 20:08:25 -0400

    Adding details to recipe page

--- src/components/pages/Recipe.jsx
+++ src/components/pages/Recipe.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { styled } from '@mui/material/styles';
-import { Box, Stack, Typography, Paper } from '@mui/material';
+import { Box, Stack, Typography, Paper, Rating } from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import PersonIcon from '@mui/icons-material/Person';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import LunchDiningIcon from '@mui/icons-material/LunchDining';
+import LunchDiningOutlinedIcon from '@mui/icons-material/LunchDiningOutlined';
 
 import { Navbar } from '../../components';
 import recipes from '../../data/recipes';
@@ -14,7 +19,7 @@ const RecipePaper = styled(Paper)(() => ({
 
 const SectionStack = styled(Stack)(() => ({
     display: 'flex',
-    gap: '2rem'
+    gap: '2.5rem'
 }));
 
 const SectionTitle = styled(Typography)(() => ({
@@ -29,6 +34,22 @@ const SectionElement = styled(Typography)(() => ({
     color: '#fff',
 }));
 
+const DetailBox = styled(Box)(() => ({
+    display: 'flex',
+    alignItems: 'center',
+    gap: 4
+}));
+
+const CardText = styled(Typography)(() => ({
+    color: '#d9d9d9',
+}));
+
+const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+        color: '#ff8533',
+    },
+});
+
 const Recipe = () => {
 
     // Constants
@@ -39,7 +60,12 @@ const Recipe = () => {
     return (
         <>
             <Navbar />
-            <Box sx={{ mx: '24rem', mb: '6rem' }}>
+            <Box
+                sx={{
+                    mx: '24rem',
+                    mb: '6rem'
+                }}
+            >
                 <RecipePaper elevation={12}>
                     <SectionStack>
                         {recipe ? (
@@ -48,10 +74,53 @@ const Recipe = () => {
                                     display="flex"
                                     justifyContent="space-between"
                                 >
-                                    <Typography className='kepo-h3-orange'>
-                                        {recipe.recipeName}
-                                    </Typography>
-                                    <img className="" src={recipe.photo} height={200}/>
+                                    <Stack
+                                        display="flex"
+                                        gap={1.5}
+                                    >
+                                        <Typography
+                                            className='kepo-h3-orange'
+                                            sx={{
+                                                width: '100%',
+                                                mb: 0.5,
+                                            }}
+                                        >
+                                            {recipe.recipeName}
+                                        </Typography>
+                                        <DetailBox>
+                                            <AccessTimeIcon fontSize='' sx={{ color: '#fff' }} />
+                                            <CardText>
+                                                Prep time <span className='recipe-det'>(min)</span>&nbsp;&nbsp;&nbsp;<span className='recipe-detail-value'>{recipe.prepTime}</span>
+                                            </CardText>
+                                        </DetailBox>
+                                        <DetailBox>
+                                            <AccessTimeIcon fontSize='' sx={{ color: '#fff' }} />
+                                            <CardText>
+                                                Cook time <span className='recipe-det'>(min)</span>&nbsp;&nbsp;&nbsp;<span className='recipe-detail-value'>{recipe.cookTime}</span>
+                                            </CardText>
+                                        </DetailBox>
+                                        <DetailBox>
+                                            <PersonIcon fontSize='' sx={{ color: '#fff' }} />
+                                            <CardText>
+                                                Serves&nbsp;&nbsp;&nbsp;<span className='recipe-detail-value'>{recipe.serves}</span>
+                                            </CardText>
+                                        </DetailBox>
+                                        <DetailBox>
+                                            <LocalDiningIcon fontSize='' sx={{ color: '#fff' }} />
+                                            <CardText>
+                                                Difficulty&nbsp;&nbsp;
+                                            </CardText>
+                                            <StyledRating
+                                                readonly
+                                                size="small"
+                                                value={recipe.difficulty}
+                                                precision={0.5}
+                                                icon={<LunchDiningIcon fontSize="inherit" />}
+                                                emptyIcon={<LunchDiningOutlinedIcon fontSize="inherit" />}
+                                            />
+                                        </DetailBox>
+                                    </Stack>
+                                    <img className="recipe-photo" src={recipe.photo}/>
                                 </Box>
                                 <Box>
                                     <SectionTitle>Ingredients</SectionTitle>
