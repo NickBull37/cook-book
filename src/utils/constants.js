64eedff0153c5382dd6cb06ad3d33ef9a8cb8579
Nick <nbulisky@gmail.com>
2025-06-06 18:15:22 -0400

    adding more recipes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,3 +13,6 @@ export const watermelonMarg = require('../images/recipes/watermelon-marg.jpeg');
 export const burgerSauce = require('../images/recipes/burger-sauce.png');
 export const steakAioli = require('../images/recipes/steak-aioli.jpg');
 export const cowboyButter = require('../images/recipes/cowboy-butter.jpg');
+export const crabToppedSalmon = require('../images/recipes/crab-topped-salmon.jpg');
+export const greekSalad = require('../images/recipes/greek-salad.jpg');
+export const chickenTacos = require('../images/recipes/chicken-tacos.jpg');
